1c883b86bc768786a3845d29e6031be7b44bf0d5
nahuy-online <nahuy.online@gmail.com>
2025-12-14 08:33:10 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ВАЖНО: base должен совпадать с названием вашего репозитория на GitHub
-  base: '/nahuy-NFT-store/',
+  base: '/',
   build: {
     outDir: 'dist',
   }
